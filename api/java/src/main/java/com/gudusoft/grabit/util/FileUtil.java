e59305e6a6c7d15985a354f30842114fd90fedc1
shenhuan1006 <33363287+shenhuan1006@users.noreply.github.com>
2021-03-13 15:26:42 +0800

    Create FileUtil.java

---
+++ api/java/src/main/java/com/gudusoft/grabit/util/FileUtil.java
@@ -0,0 +1,89 @@
+package com.gudusoft.grabit.util;
+
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class FileUtil {
+
+    private static final int BUFFER_SIZE = 10 * 1024 * 1024;
+
+    private FileUtil() {
+    }
+
+    public static void mkFile(String filePath) throws IOException {
+        File testFile = new File(filePath);
+        File fileParent = testFile.getParentFile();
+        if (!fileParent.exists()) {
+            fileParent.mkdirs();
+        }
+        if (!testFile.exists()) {
+            testFile.createNewFile();
+        }
+    }
+
+    public static void toZip(String srcDir, OutputStream out, boolean KeepDirStructure)
+            throws RuntimeException {
+        ZipOutputStream zos = null;
+        try {
+            zos = new ZipOutputStream(out);
+            File sourceFile = new File(srcDir);
+            compress(sourceFile, zos, sourceFile.getName(), KeepDirStructure);
+        } catch (Exception e) {
+            throw new RuntimeException("zip error from ZipUtils", e);
+        } finally {
+            if (zos != null) {
+                try {
+                    zos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    private static void compress(File sourceFile, ZipOutputStream zos, String name,
+                                 boolean KeepDirStructure) throws Exception {
+        byte[] buf = new byte[BUFFER_SIZE];
+        if (sourceFile.isFile()) {
+            zos.putNextEntry(new ZipEntry(name));
+            int len;
+            FileInputStream in = new FileInputStream(sourceFile);
+            while ((len = in.read(buf)) != -1) {
+                zos.write(buf, 0, len);
+            }
+            zos.closeEntry();
+            in.close();
+        } else {
+            File[] listFiles = sourceFile.listFiles();
+            if (listFiles == null || listFiles.length == 0) {
+                if (KeepDirStructure) {
+                    zos.putNextEntry(new ZipEntry(name + "/"));
+                    zos.closeEntry();
+                }
+
+            } else {
+                for (File file : listFiles) {
+                    if (KeepDirStructure) {
+                        compress(file, zos, name + "/" + file.getName(), KeepDirStructure);
+                    } else {
+                        compress(file, zos, file.getName(), KeepDirStructure);
+                    }
+                }
+            }
+        }
+    }
+
+    public static OutputStream outStream(String path) throws IOException {
+        FileOutputStream fileOutputStream;
+        try {
+            fileOutputStream = new FileOutputStream(path);
+        } catch (Exception ex) {
+            mkFile(path);
+            fileOutputStream = new FileOutputStream(path);
+        }
+        return fileOutputStream;
+    }
+
+}
