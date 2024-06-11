0cf11eb7ec0cfe5fa2cf4cdc6e60d1eea44402a7
shenhuan2021 <33363287+shenhuan2021@users.noreply.github.com>
2024-06-11 21:27:03 +0800

    Update GenerateTokenDemo.py

--- api/python/basic/GenerateTokenDemo.py
+++ api/python/basic/GenerateTokenDemo.py
@@ -2,9 +2,34 @@ import requests
 
 import json
 
+def getToken(userId, server, port, screctKey):
+    if userId == 'gudu|0123456789':
+        return 'token'
+    url = '/api/gspLive_backend/user/generateToken'
+    if port != '':
+        url = server + ':' + port + url
+    else:
+        url = server + url
+    mapA = {'secretKey': screctKey, 'userId': userId}
+    header_dict = {"Content-Type": "application/x-www-form-urlencoded"}
+
+    try:
+        r = requests.post(url, data=mapA, headers=header_dict)
+    except Exception as e:
+        print('get token failed.', e)
+    result = json.loads(r.text)
+
+    if result['code'] == '200':
+        return result['token']
+    else:
+        print(result['error'])
+
+
 if __name__ == '__main__':
 
-    url = 'https://api.gudusoft.com/gspLive_backend/user/generateToken'
+    server = ''
+
+    port = ''
 
     # the user id of sqlflow web or client, required true
     userId = ''
@@ -12,9 +37,6 @@ if __name__ == '__main__':
     # the secret key of sqlflow user for webapi request, required true
     screctKey = ''
 
-    mapA = {'secretKey': screctKey, 'userId': userId}
-    header_dict = {"Content-Type": "application/x-www-form-urlencoded"}
+    token = getToken(userId, server, port, screctKey)
 
-    r = requests.post(url, data=mapA, headers=header_dict)
-
-    print(r.text)
+    print(token)
